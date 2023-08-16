3a6b66d0d9817fa5740067e0b671bae73a9bc2ad
bwconrad <benwconrad@gmail.com>
2023-08-16 17:37:47 -0300

    Fix import typo

--- soft_moe_pytorch/__init__.py
+++ soft_moe_pytorch/__init__.py
@@ -1,4 +1,4 @@
 from .soft_moe import SoftMoELayerWrapper
-from .vision_transformer import (SoftMOEVisionTransformer, soft_moe_vit_base,
+from .vision_transformer import (SoftMoEVisionTransformer, soft_moe_vit_base,
                                  soft_moe_vit_huge, soft_moe_vit_large,
                                  soft_moe_vit_small, soft_moe_vit_tiny)
